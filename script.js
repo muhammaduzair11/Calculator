b2996fda903ff06c818a49facbe1838c9c598eba
muhammadusmanmunir <muhammaduzairmuhammaduzair11@gmail.com>
2021-08-22 21:00:01 +0500

    complete-but-with-some-minor-bugs

--- script.js
+++ script.js
@@ -4,26 +4,57 @@ let operators = document.querySelectorAll('.operator');
 let clear = document.querySelector('.clear');
 let isEqual = document.querySelector('.equal');
 let previousNumber;
-parseInt(result)
-parseInt(previousNumber);
-
+let currentNumber;
+let operatorSaver;
+parseInt(result.innerText)
+// parseInt(result)
+// Numbers buttons
 numbers.forEach(item => {
   item.addEventListener("click", () => {
-    result.innerText += item.innerText;
-    console.log(typeof (parseInt(result)));
+    // console.log(typeof (parseInt(result)));
+    if (result.innerText === '+' || result.innerText === '-' || result.innerText === 'X' || result.innerText === '÷') {
+      result.innerText = null;
+    } else {
+      result.innerText += item.innerText;
+    }
   })
 });
-
+// Operator buttons +, -, x, ÷
 operators.forEach(item => {
   item.addEventListener("click", () => {
-    previousNumber = result.innerText;
+    previousNumber = parseInt(result.innerText);
+    result.innerText = null;
+    result.innerText += item.innerText;
+    operatorSaver = item.innerText;
+
   })
 });
+parseInt(previousNumber);
 
+// Equal Button = 
 isEqual.addEventListener("click", () => {
-  result.innerText = parseInt(result.innerText) + 2;
+  currentNumber = parseInt(result.innerText);
+  switch (operatorSaver) {
+    case "X":
+      result.innerText = previousNumber * currentNumber;
+      break;
+    case "+":
+      result.innerText = previousNumber + currentNumber;
+      break;
+    case "-":
+      result.innerText = previousNumber - currentNumber;
+      break;
+    case "÷":
+      result.innerText = previousNumber / currentNumber;
+      break;
+    default:
+      result.innerText = result.innerText = 'Something went wrong';
+  };
 })
-
+// Equal button
 clear.addEventListener("click", () => {
   result.innerText = null;
+  previousNumber = null;
+  operatorSaver = null;
+  currentNumber = null;
 })
