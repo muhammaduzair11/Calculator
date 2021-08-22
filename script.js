d09df1b211eb8d188c46b4fbb5344f5eeb3f288d
muhammadusmanmunir <muhammaduzairmuhammaduzair11@gmail.com>
2021-08-22 17:19:49 +0500

    JS-with-lot-of-bugs

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const number = document.querySelectorAll('number');
-const result = document.querySelector
+const numbers = document.querySelectorAll('.number');
+let result = document.querySelector('.result');
+let operators = document.querySelectorAll('.operator');
+let clear = document.querySelector('.clear');
+let isEqual = document.querySelector('.equal');
+let previousNumber;
+parseInt(result)
+parseInt(previousNumber);
+
+numbers.forEach(item => {
+  item.addEventListener("click", () => {
+    result.innerText += item.innerText;
+    console.log(typeof (parseInt(result)));
+  })
+});
+
+operators.forEach(item => {
+  item.addEventListener("click", () => {
+    previousNumber = result.innerText;
+  })
+});
+
+isEqual.addEventListener("click", () => {
+  result.innerText = parseInt(result.innerText) + 2;
+})
+
+clear.addEventListener("click", () => {
+  result.innerText = null;
+})
